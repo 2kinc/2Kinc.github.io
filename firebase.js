0996a416aa43199f463514a783cca59611815985
Ken Zhou <mailofrealcev@gmail.com>
2019-02-20 14:38:37 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDMuGYsD_rUZUBRDQeKvQiWW7jo7gOuW-k",
+  authDomain: "exp-game.firebaseapp.com",
+databaseURL: "https://exp-game.firebaseio.com",
+projectId: "exp-game",
+storageBucket: "exp-game.appspot.com",
+messagingSenderId: "770010726831"
+};
+firebase.initializeApp(config);
+
 var actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be whitelisted in the Firebase Console.
