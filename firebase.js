9f640cfeacab977a170bbc54bd53cafdc2058958
Ken Zhou <mailofrealcev@gmail.com>
2019-02-20 14:58:49 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -16,13 +16,12 @@ var config = {
 firebase.initializeApp(config);
 
 var actionCodeSettings = {
-  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL you want to redirect back to.
   // URL must be whitelisted in the Firebase Console.
   url: 'https://2kinc.github.io/callback',
   // This must be true.
   handleCodeInApp: true
 };
-
 
 firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
   .then(function() {
