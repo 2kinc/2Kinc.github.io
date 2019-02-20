1a3e2379728d09a003da447ab239180fe7dcaae3
Ken Zhou <mailofrealcev@gmail.com>
2019-02-20 14:37:20 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,7 @@
+var actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be whitelisted in the Firebase Console.
+  url: 'https://2kinc.github.io/callback',
+  // This must be true.
+  handleCodeInApp: true
+};
