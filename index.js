bc363ee92a1d0c70c2ca374e0b395b22f115bb3f
Ken Zhou <mailofrealcev@gmail.com>
2019-03-07 20:32:58 -0500

    extremely important commit: some code was deleted on a merge and has been recovered in this commit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function Site() {
         submitpost: $('#submit-post-button'),
         signinwithgoogle: $('#sign-in-with-google'),
         userinfo: $('#user-info'),
-        globalchatdisabled: $('#global-chat-disabled')
+        globalchatdisabled: $('#global-chat-disabled'),
+        yourpropic: $('#your-pro-pic'),
+        bigbanner: $('#big-banner')
     };
     this.displayMessage = function(m) {
         var p = document.createElement('p');
@@ -31,15 +33,95 @@ function Site() {
         var button = document.createElement('button');
         button.className = 'mdc-button mdc-ripple-upgraded mdc-button--unelevated';
         button.innerText = 'Log out of 2K inc';
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             auth.signOut();
             location.reload();
         });
         this.elements.userinfo.html('');
         this.elements.userinfo.text('You are signed in as ' + auth.currentUser.displayName + '. ');
         this.elements.userinfo.append(button);
+        this.elements.yourpropic.attr('src', auth.currentUser.photoURL);
+        this.elements.yourpropic.show();
+        this.elements.signinwithgoogle.hide();
         this.user = auth.currentUser;
+    };
+    this.DropdownMenu = function(title, items, coordinate) {
+        //items is an array like this:
+        //[{name: 'OPTION NAME', handler: handler()}]
+        //coordinate is an object with {x: ?, y: ?}, x and y are numbers
+        //usually just use event.clientX and event.clientY
+        this.el = document.createElement('div');
+        this.el.className = 'dropdown-menu mdc-card';
+        this.el.style.display = 'none';
+        var ul = document.createElement('ul');
+        ul.className = 'mdc-list';
+        var titleEl = document.createElement('div');
+        titleEl.className = 'dropdown-menu-title';
+        titleEl.innerText = title;
+        this.el.appendChild(titleEl);
+        this.primed = false;
+        var that = this;
+        items.forEach(function(element) {
+            var li = document.createElement('li');
+            li.classList.add('mdc-list-item');
+            var span = document.createElement('span');
+            span.classList.add('mdc-list-item__text');
+            span.innerText = element.name;
+            li.appendChild(span);
+            that.el.appendChild(li);
+
+            function clickHandler() {
+                if (that.primed == true) {
+                    element.handler();
+                } else {
+                    that.primed = true;
+                }
+            }
+
+            li.onclick = clickHandler;
+        });
+        if (coordinate.x < 0) {
+            this.el.style.right = Math.abs(coordinate.x) + 'px';
+        } else {
+            this.el.style.left = coordinate.x + 'px';
+        }
+
+        if (coordinate.y < 0) {
+            this.el.style.bottom = Math.abs(coordinate.y) + 'px';
+        } else {
+            this.el.style.top = coordinate.y + 'px';
+        }
+
+        this.show = function() {
+            this.el.style.display = 'block';
+        };
+        this.hide = function() {
+            this.el.style.display = 'none';
+        };
+        this.delete = function() {
+            if (this.el.parentNode != null)
+                document.body.removeChild(this.el);
+            this.primed = false;
+        }
+
+        document.body.appendChild(this.el);
+    };
+
+    this.toggleProfileDropdownMenu = false;
+
+    this.profileDropdownMenu;
+
+    this.rightClickDropdownMenu;
+
+    this.elements.bigbanner.typedjsOptions = {
+        strings: ['2K inc.', 'games++', 'levels++', 'xp++', 'skills++', 'We are awesome.', 'We are ^600 2K inc.'],
+        typeSpeed: 45,
+        smartDelay: 100,
+        backSpeed: 25,
+        backDelay: 600
     }
+
+    this.elements.bigbanner.typed = new Typed('#big-banner', this.elements.bigbanner.typedjsOptions);
 }
 
 var site = new Site();
@@ -118,4 +200,61 @@ auth.onAuthStateChanged(function(user) {
         site.elements.userinfo.text("You are not signed in to 2K inc. Sign in to save your games and join the party in the Global Chat!");
         site.elements.globalchatdisabled.show();
     }
+});
+
+site.elements.yourpropic.click(function() {
+    if (site.toggleProfileDropdownMenu) {
+        site.profileDropdownMenu.delete();
+        site.toggleProfileDropdownMenu = false;
+    } else {
+        site.profileDropdownMenu = new site.DropdownMenu(
+            'Profile',
+            [{
+                    name: 'Username: ' + site.user.displayName,
+                    handler: function() {}
+                },
+                {
+                    name: 'Email: ' + site.user.email,
+                    handler: function() {}
+                }
+            ], {
+                x: -10,
+                y: site.elements.yourpropic.position().top + 55
+            });
+        site.profileDropdownMenu.el.id = 'profile-dropdown-menu';
+        site.profileDropdownMenu.show();
+        site.toggleProfileDropdownMenu = true;
+    }
+});
+
+document.oncontextmenu = function(e) {
+    e.preventDefault();
+    if (site.rightClickDropdownMenu != undefined) {
+        if (site.rightClickDropdownMenu.el.parentNode != null)
+            site.rightClickDropdownMenu.delete();
+    }
+    site.rightClickDropdownMenu = new site.DropdownMenu(
+        'Action Menu',
+        [{
+                name: 'Copy',
+                handler: document.execCommand('copy')
+            },
+            /*{
+              name: 'Reload',
+              handler: location.reload()
+            },*/
+            /*{
+              name: 'Go to Global Chat',
+              handler: $('#post-stuff-button').trigger('click')
+            }*/
+        ], {
+            x: e.clientX + 5,
+            y: e.clientY + 5
+        });
+    site.rightClickDropdownMenu.show();
+    return false;
+};
+
+$(document.body).not(".dropdown-menu").click(function(e) {
+    site.rightClickDropdownMenu.delete();
 });
